b182df2340c688311e0081c04e97f13926f488ce
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2026-02-06 11:05:21 +0000

    updated response.py

--- app/models/response.py
+++ app/models/response.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
+from typing import List
 
 class AnalyzeResponse(BaseModel):
     filename: str
     text_length: int
 
+class ATSScoreResponse(BaseModel):
+    filename: str
+    detected_role: str
+    score: int
+    missing_keywords: List[str]
+    suggestions: List[str]
