5f7fc1ba9056cc81ce3988112d645089a996285c
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2026-02-06 10:24:45 +0000

    added loggging.py

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,7 +1,19 @@
 import logging
+import json
+
+class JsonFormatter(logging.Formatter):
+    def format(self, record):
+        if isinstance(record.msg, dict):
+            record.msg = json.dumps(record.msg)
+        return super().format(record)
 
 def setup_logging(level: str):
-    logging.basicConfig(
-        level=level,
-        format="%(asctime)s | %(levelname)s | %(message)s"
+    handler = logging.StreamHandler()
+    formatter = JsonFormatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
     )
+    handler.setFormatter(formatter)
+
+    root = logging.getLogger()
+    root.setLevel(level)
+    root.handlers = [handler]
